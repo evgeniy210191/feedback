ae022e2085b5fb635819f9660b69fd1d76ce03f6
evgeniy210191 <bees127@ukr.net>
2023-12-21 18:05:43 +0200

    add color bg

--- src/constans/color.js
+++ src/constans/color.js
@@ -2,7 +2,7 @@ export const theme = {
   color: {
     colorTextCommom: '#242424',
     colorTextSecond: '#010101',
-    backgroundCommen: '#00ffff',
+    backgroundCommen: '#e8f4f3',
     backgroundButton: '#ffff00',
     shadeElem: '#00ff00',
   },
